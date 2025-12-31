dc11cea536f0a370502a018832c02d72354a1859
Shayan Halder <shayanhalder2389@gmail.com>
2025-12-31 11:01:28 +0530

    update base path in vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   define: {
     'process.env.VITE_KEY': JSON.stringify(process.env.VITE_KEY)
 
